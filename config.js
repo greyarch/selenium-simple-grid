a1f415dae28f75f0917eae21b1d27f268eeab775
greyarch <gantcho@greyarch.net>
2012-04-17 16:20:16 +0200

    server config separated

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.servers = [
+    {"name" : "selenium-slave18", "host" : "10.19.55.173", "port" : "4444"},
+    {"name" : "selenium-slave19", "host" : "10.19.55.174", "port" : "4444"},
+    {"name" : "selenium-slave20", "host" : "10.19.55.175", "port" : "4444"},
+    {"name" : "selenium-slave21", "host" : "10.19.55.176", "port" : "4444"}
+]
